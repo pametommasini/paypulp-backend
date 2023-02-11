7a4cc036414898224391b9abdb43519086c69e4c
JavierRico19 <javier.rico.moreno@gmail.com>
2023-02-11 09:50:21 +0100

    jwt

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.use('/users', require("./users"));
 
 router.get('/', require('../controllers/secureRequestController'));
 
+//endpoint de login
+router.post('/login', require('../controllers/loginController'));
 
+// endpoint de signup
+router.post('/signup', require('../controllers/signupController'));
 
 module.exports = router;
