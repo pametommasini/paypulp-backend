1231fb0ad84eb4e15b58c4e7392f5763698ba36b
DanNisenson <danpablo1@gmail.com>
2023-02-14 20:15:40 +0100

    change signup body data to camelCase. clean console.logs

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,15 +1,12 @@
 const jwt = require('jsonwebtoken');
 const newClient = require("../model/newClient");
 const CryptoJS = require("crypto-js");
-const MD5 = require("crypto-js/md5");
-
 
 const loginController = async (req, res) => {
   const client = await newClient();
   const { email, password } = req.body;
   const md5Password = CryptoJS.MD5(password).toString();
   const dbClient = await client.query("SELECT * FROM users WHERE email = ($1)", [email]);
-  console.log("HOLAAAAAA", dbClient)
   if(dbClient.rows.length === 0){
     return res.status(401).json("User not found!").end();
   }
--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,35 +1,35 @@
 const jwt = require('jsonwebtoken');
 const newClient = require("../model/newClient");
 const CryptoJS = require("crypto-js");
-const MD5 = require("crypto-js/md5");
 const { v4: uuidv4 } = require('uuid');
 
 const signupController = async (req, res) => {
     const client = await newClient();
-    const user_uuid = uuidv4();
+    const userUuid = uuidv4();
     const { email,
         password,
-        account_type,
-        first_name,
-        last_name,
+        accountType,
+        firstName,
+        lastName,
         phone,
-        birth_date,
+        birthDate,
         address,
         city,
         country,
-        time_zone,
-        security_question,
-        security_question_answer
+        timeZone,
+        securityQuestion,
+        securityQuestionAnswer
     } = req.body;
     const md5Password = CryptoJS.MD5(password).toString();
+    console.log(req.body);
     let resUsers;
      try {
         const dbUsers = await client.query('INSERT INTO users (user_uuid, email, account_type, password) VALUES (($1), ($2), ($3), ($4)) RETURNING *',
-            [user_uuid, email, account_type, md5Password]);
+            [userUuid, email, accountType, md5Password]);
             resUsers = dbUsers.rows[0];
     } catch (error) {
-        console.log(error);
-        res.status(401).json("Incomplete user information!").end;
+        console.log(error, "1");
+        return res.status(401).json("Incomplete user information!").end;
     } 
     //creation time
     const today = new Date();
@@ -39,18 +39,18 @@ const signupController = async (req, res) => {
     try {
         const dbCustomers = await client.query(
             'INSERT INTO paypulp_costumers (user_uuid, first_name, last_name, phone, birth_date, address, city, country, time_zone, security_question, security_question_answer, creation_time) VALUES (($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12)) RETURNING *',
-            [user_uuid, first_name, last_name, phone, birth_date, address, city, country, time_zone, security_question, security_question_answer, creation_time]);
+            [userUuid, firstName, lastName, phone, birthDate, address, city, country, timeZone, securityQuestion, securityQuestionAnswer, creation_time]);
             resCustomers = dbCustomers.rows[0];
     } catch (error) {
-        console.log(error);
-        res.status(401).json("Incomplete customer information!").end;
+        console.log(error, "2");
+        return res.status(401).json("Incomplete customer information!").end;
     }
     delete resUsers.password;
     const token = jwt.sign({ email }, process.env.SECRET, {
         algorithm: 'HS256',
         expiresIn: 3000
     })
-    res.status(200).json({ token, resCustomers, resUsers });
+    res.status(200).json({ token, resUsers, resCustomers });
 }
 
 module.exports = signupController;
