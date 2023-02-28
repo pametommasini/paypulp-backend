0fa36d65243e45d783021e5a5324be13ade1b4ff
DanNisenson <danpablo1@gmail.com>
2023-02-28 22:23:15 +0100

    add error handling for getTransaction

--- model/transaction.js
+++ model/transaction.js
@@ -38,6 +38,7 @@ class transaction {
       const queryRes = await pgClient.query ( 
         "SELECT transactions.transaction_id, transactions.personal_id,transactions.business_id, transactions.total_amount, transactions.date_time, transactions.went_trough,paypulp_costumers.user_uuid, paypulp_costumers.first_name, business_accounts.business_name, products.product_name, payment_methods.card_number, payment_methods.is_preferred FROM transactions INNER JOIN personal_accounts ON transactions.personal_id = personal_accounts.personal_id INNER JOIN business_accounts ON transactions.business_id = business_accounts.business_id INNER JOIN products ON transactions.product_uuid = products.product_uuid  INNER JOIN payment_methods ON transactions.pay_method_uuid = payment_methods.pay_method_uuid INNER JOIN paypulp_costumers ON business_accounts.costumer_id = paypulp_costumers.costumer_id OR personal_accounts.costumer_id = paypulp_costumers.costumer_id WHERE user_uuid = ($1) ORDER BY date_time DESC;", [userUuid]
       );
+      if (queryRes.rows === 0) res.status(400).json({error: "no transactions found"})
         pgClient.end();
       return queryRes.rows;
     };
