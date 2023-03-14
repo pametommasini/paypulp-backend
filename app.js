83a4be0a05cc953220f7fdaab11f940b28c607da
DanNisenson <danpablo1@gmail.com>
2023-03-14 16:56:45 +0100

    🔨Port fixed to 8080

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const createError = require("http-errors");
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
@@ -21,22 +20,6 @@ app.use('/private', validateToken, require('./routes'))
 
 // public routes
 app.post('/login', loginValidFields, loginValid, require('./controllers/Auth/loginController'))
-app.post('/signup', signupValidFields, validateResult, require('./controllers/Auth/signupController'))
-
-/* // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-}); */
+app.post('/signup', signupValidFields, validateResult, require('./controllers/Auth/signupController'))\
 
 module.exports = app
