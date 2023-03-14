6a3f65858d8018712e03d9779ef53682e417b66e
DanNisenson <danpablo1@gmail.com>
2023-03-14 22:32:09 +0100

    👷WIP: comment out transaction unused endpoints

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const { validateTransaction } = require('../middlewares/TransactionValidator');
+const { validateResult } = require('../middlewares/validators/bodyValid');
 
 // router.get('/', require("../controllers/Transactions/getAllTransactions"));
 
-router.get('/', require("../controllers/Transactions/getTransaction"));
+// router.get('/', require("../controllers/Transactions/getTransaction"));
 
-router.post('/', validateTransaction, require("../controllers/Transactions/getNewTransaction"));
+router.post('/:selleruuid/:totalamount', validateTransaction, validateResult, require("../controllers/Transactions/getNewTransaction"));
 
 module.exports = router;
