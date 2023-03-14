95ba7eef2fa11e54d54c41ea986bd3f6acae872b
DanNisenson <danpablo1@gmail.com>
2023-03-14 19:11:16 +0100

    🔨Turn dbV2 to snake_case

--- model/dbV2.sql
+++ model/dbV2.sql
@@ -1,79 +1,79 @@
-CREATE DATABASE "paypulpV2";
+CREATE DATABASE "paypulp_v2";
 
 CREATE TABLE "users" (
-	"userId" serial NOT NULL UNIQUE,
-	"userUuid" varchar(40) NOT NULL UNIQUE,
+	"user_id" serial NOT NULL UNIQUE,
+	"user_uuid" varchar(40) NOT NULL UNIQUE,
 	"email" varchar(80) NOT NULL UNIQUE,
-	"accountType" varchar(15) NOT NULL,
 	"password" varchar(32) NOT NULL,
-	"firstName" varchar(50) NOT NULL,
-	"funds" numeric NOT NULL,
-	CONSTRAINT "usersPk" PRIMARY KEY ("userId")
+	"first_name" varchar(50) NOT NULL,
+	"account_type" varchar(15) NOT NULL DEFAULT 'personal',
+	"funds" numeric NOT NULL DEFAULT '0',
+	CONSTRAINT "users_pk" PRIMARY KEY ("user_id")
 );
 
 
 
-CREATE TABLE "personalInfo" (
-	"personalInfoId" serial NOT NULL UNIQUE,
-	"userUuid" varchar(40) NOT NULL UNIQUE,
-	"lastName" varchar(50) NOT NULL,
+CREATE TABLE "personal_info" (
+	"personal_info_id" serial NOT NULL UNIQUE,
+	"user_uuid" varchar(40) NOT NULL UNIQUE,
+	"last_name" varchar(50) NOT NULL,
 	"phone" varchar(20) NOT NULL,
-	"birthDate" DATE NOT NULL,
+	"birth_date" DATE NOT NULL,
 	"address" varchar(100) NOT NULL,
 	"city" varchar(50) NOT NULL,
 	"country" varchar(50) NOT NULL,
-	"timeZone" varchar(10) NOT NULL,
-	"creationTime" DATE NOT NULL,
-	CONSTRAINT "personalInfoPk" PRIMARY KEY ("personalInfoId")
+	"time_zone" varchar(10) NOT NULL,
+	"creation_time" DATE NOT NULL,
+	CONSTRAINT "personal_info_pk" PRIMARY KEY ("personal_info_id")
 );
 
 
 
-CREATE TABLE "sellerInfo" (
-	"sellerInfoId" serial NOT NULL UNIQUE,
-	"userUuid" varchar(40) NOT NULL UNIQUE,
-	"sellerName" varchar(50) NOT NULL UNIQUE,
+CREATE TABLE "seller_info" (
+	"seller_info_id" serial NOT NULL UNIQUE,
+	"user_uuid" varchar(40) NOT NULL UNIQUE,
+	"seller_name" varchar(50) NOT NULL UNIQUE,
 	"category" varchar(30) NOT NULL,
-	"storeAddress" varchar(50),
-	"storeAddressAddInfo" varchar(50) NOT NULL,
-	CONSTRAINT "sellerInfoPk" PRIMARY KEY ("sellerInfoId")
+	"store_address" varchar(50),
+	"store_address_add_info" varchar(50) NOT NULL,
+	CONSTRAINT "seller_info_pk" PRIMARY KEY ("seller_info_id")
 );
 
 
 
-CREATE TABLE "paymentMethods" (
-	"payMethodId" serial NOT NULL UNIQUE,
-	"userUuid" varchar(40) NOT NULL,
-	"payMethodUuid" varchar(40) NOT NULL UNIQUE,
-	"cardNumber" varchar(25) NOT NULL,
-	"cardOwnerName" varchar(50) NOT NULL,
-	"cardExpiryDate" DATE NOT NULL,
-	"cardCvv" varchar(4) NOT NULL,
-	CONSTRAINT "paymentMethodsPk" PRIMARY KEY ("payMethodId")
+CREATE TABLE "payment_methods" (
+	"pay_method_id" serial NOT NULL UNIQUE,
+	"user_uuid" varchar(40) NOT NULL,
+	"payment_method_uuid" varchar(40) NOT NULL UNIQUE,
+	"card_number" varchar(25) NOT NULL,
+	"card_owner_name" varchar(50) NOT NULL,
+	"card_expiry_date" DATE NOT NULL,
+	"card_cvv" varchar(4) NOT NULL,
+	CONSTRAINT "payment_methods_pk" PRIMARY KEY ("pay_method_id")
 );
 
 
 
 CREATE TABLE "transactions" (
-	"transactionId" serial NOT NULL UNIQUE,
-	"sellerUuid" varchar(40) NOT NULL,
-	"buyerUuid" varchar(40) NOT NULL,
-	"payMethodUuid" varchar(40) NOT NULL,
-	"totalAmount" numeric NOT NULL,
-	"dateTime" TIMESTAMP NOT NULL,
-	"wentTrough" BOOLEAN NOT NULL,
-	CONSTRAINT "transactionsPk" PRIMARY KEY ("transactionId")
+	"transaction_id" serial NOT NULL UNIQUE,
+	"seller_uuid" varchar(40) NOT NULL,
+	"buyer_uuid" varchar(40) NOT NULL,
+	"payment_method_uuid" varchar(40) NOT NULL,
+	"total_amount" numeric NOT NULL,
+	"date_time" TIMESTAMP NOT NULL,
+	"went_trough" BOOLEAN NOT NULL,
+	CONSTRAINT "transactions_pk" PRIMARY KEY ("transaction_id")
 );
 
 
 
 
-ALTER TABLE "personalInfo" ADD CONSTRAINT "personalInfoFk0" FOREIGN KEY ("userUuid") REFERENCES "users"("userUuid");
+ALTER TABLE "personal_info" ADD CONSTRAINT "personal_info_fk0" FOREIGN KEY ("user_uuid") REFERENCES "users"("user_uuid");
 
-ALTER TABLE "sellerInfo" ADD CONSTRAINT "sellerInfoFk0" FOREIGN KEY ("userUuid") REFERENCES "users"("userUuid");
+ALTER TABLE "seller_info" ADD CONSTRAINT "seller_info_fk0" FOREIGN KEY ("user_uuid") REFERENCES "users"("user_uuid");
 
-ALTER TABLE "paymentMethods" ADD CONSTRAINT "paymentMethodsFk0" FOREIGN KEY ("userUuid") REFERENCES "users"("userUuid");
+ALTER TABLE "payment_methods" ADD CONSTRAINT "payment_methods_fk0" FOREIGN KEY ("user_uuid") REFERENCES "users"("user_uuid");
 
-ALTER TABLE "transactions" ADD CONSTRAINT "transactionsFk0" FOREIGN KEY ("sellerUuid") REFERENCES "users"("userUuid");
-ALTER TABLE "transactions" ADD CONSTRAINT "transactionsFk1" FOREIGN KEY ("buyerUuid") REFERENCES "users"("userUuid");
-ALTER TABLE "transactions" ADD CONSTRAINT "transactionsFk2" FOREIGN KEY ("payMethodUuid") REFERENCES "paymentMethods"("payMethodUuid");
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_fk0" FOREIGN KEY ("seller_uuid") REFERENCES "users"("user_uuid");
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_fk1" FOREIGN KEY ("buyer_uuid") REFERENCES "users"("user_uuid");
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_fk2" FOREIGN KEY ("payment_method_uuid") REFERENCES "payment_methods"("payment_method_uuid");
