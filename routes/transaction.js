212e81a48dff115b7cb5b15db72d9b89dae57d63
DanNisenson <danpablo1@gmail.com>
2023-03-14 22:32:41 +0100

    👷WIP: comment out transaction unused endpoints

--- routes/transaction.js
+++ routes/transaction.js
@@ -7,6 +7,6 @@ const { validateResult } = require('../middlewares/validators/bodyValid');
 
 // router.get('/', require("../controllers/Transactions/getTransaction"));
 
-router.post('/:selleruuid/:totalamount', validateTransaction, validateResult, require("../controllers/Transactions/getNewTransaction"));
+// router.post('/:selleruuid/:totalamount', validateTransaction, validateResult, require("../controllers/Transactions/getNewTransaction"));
 
 module.exports = router;
