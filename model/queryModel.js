816bd2514084b1bf5a468eb69355efb40f331a5c
DanNisenson <danpablo1@gmail.com>
2023-03-15 01:41:02 +0100

    🔨Function for parameter binding automation was missing

--- model/queryModel.js
+++ model/queryModel.js
@@ -5,6 +5,13 @@ const { snakizeString } = require('../helpers/casing')
 const snakeize = require('snakeize')
 
 class QueryModel {
+  /**
+   * Query for a row in a table by column
+   * @param {string} table - SQL table name
+   * @param {string} column - column name
+   * @param {string} value - column value
+   * @returns {undefined | dbData} returns undefined if query returns nothing
+   */
   static async selectBy(table, column, value) {
     const client = await dbConnect()
 
@@ -31,6 +38,12 @@ class QueryModel {
     }
   }
 
+  /**
+   * Insert multiple
+   * @param {string} table - destination table
+   * @param {object} newDataObject - columns and values to be inserted
+   * @returns {dbData}
+   */
   static async insertData(table, newDataObject) {
     const client = await dbConnect()
 
@@ -38,8 +51,10 @@ class QueryModel {
     const keys = Object.keys(snakeize(newDataObject)).toString()
     const values = Object.values(newDataObject)
 
+    const positions = this.parameterPositions(values)
+
     const query = `INSERT INTO ${tbl} (${keys}) 
-      VALUES (($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9)) 
+      VALUES (${positions}) 
       RETURNING *`
 
     try {
@@ -54,6 +69,21 @@ class QueryModel {
     } finally {
       client.end()
     }
+  }
+
+  static parameterPositions(arr) {
+    let res = ''
+
+    for (let i = 1; i <= arr.length; i++) {
+      let temp = `($${i})`
+      res += temp
+
+      if (i !== arr.length) {
+        res += ', '
+      }
+    }
+    console.log(res)
+    return res
   }
 
   static castData(data) {
