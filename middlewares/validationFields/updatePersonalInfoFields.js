31026a201ab9ff144cc656ffdd7fe5bb5e77d014
DanNisenson <danpablo1@gmail.com>
2023-03-15 14:21:33 +0100

    👷Update personal info validation to schema validation

--- middlewares/validationFields/updatePersonalInfoFields.js
+++ middlewares/validationFields/updatePersonalInfoFields.js
@@ -1,83 +1,77 @@
-const { body } = require('express-validator')
+const { checkSchema } = require('express-validator')
 
 const locale = ['es-ES']
 
-const lastName = () => {
-  return body('lastName')
-    .optional()
-    .notEmpty()
-    .withMessage('Last name param is empty!')
-    .isAlpha(locale, { ignore: ' ' })
-    .withMessage('Last name only admits letters')
-    .trim()
-}
+const persInfoSchema = checkSchema({
+  lastName: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Last name must not be empty' },
+    isString: { errorMessage: 'Last name must be a string' },
+    isAlpha: {
+      options: [locale, { ignore: ' ' }],
+      errorMessage: 'Last name must only contain letters',
+    },
+    trim: true,
+  },
+  phone: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Phone must not be empty' },
+    isInt: { errorMessage: 'Phone must be a number' },
+    trim: true,
+  },
+  birthDate: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Birth date must not be empty' },
+    // isDate: { errorMessage: 'Birth date must be a date' },
+    trim: true,
+  },
+  address: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Address must not be empty' },
+    isString: { errorMessage: 'Address must be a string' },
+    isAlphanumeric: {
+      options: [locale, { ignore: ' ' }],
+      errorMessage: 'Address must only contain letters',
+    },
+    trim: true,
+  },
+  city: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'City must not be empty' },
+    isString: { errorMessage: 'City must be a string' },
+    isAlpha: {
+      options: [locale, { ignore: ' ' }],
+      errorMessage: 'City must only contain letters',
+    },
+    trim: true,
+  },
+  country: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Country must not be empty' },
+    isString: { errorMessage: 'Country must be a string' },
+    isAlpha: {
+      options: [locale, { ignore: ' ' }],
+      errorMessage: 'Country must only contain letters',
+    },
+    trim: true,
+  },
+  timeZone: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Time zone must not be empty' },
+    isString: { errorMessage: 'Time zone must be a string' },
+    isInt: {
+      options: [locale, { ignore: ' ' }],
+      errorMessage: 'Time zone must be a number',
+    },
+    trim: true,
+  },
+})
 
-const phone = () => {
-  return body('phone')
-    .optional()
-    .notEmpty()
-    .withMessage('Phone param is empty!')
-    .isInt()
-    .withMessage('Phone field must be a number')
-    .trim()
-}
-
-const birthDate = () => {
-  return body('birthDate')
-    .optional()
-    .notEmpty()
-    .withMessage('Birth date param is empty!')
-    .isDate()
-    .withMessage('Birth date must be a valid date format')
-}
-
-const address = () => {
-  return body('address')
-    .optional()
-    .notEmpty()
-    .withMessage('Address param is empty!')
-    .isAlphanumeric(locale, { ignore: ' ' })
-    .withMessage('Address only admits letters and numbers')
-    .trim()
-}
-
-const city = () => {
-  return body('city')
-    .optional()
-    .notEmpty()
-    .withMessage('City param is empty!')
-    .isAlpha(locale, { ignore: ' ' })
-    .withMessage('City only accept letters')
-    .trim()
-}
-
-const country = () => {
-  return body('country')
-    .optional()
-    .notEmpty()
-    .withMessage('Country param is empty!')
-    .isAlpha(locale, { ignore: ' ' })
-    .withMessage('Country only accept letters')
-    .trim()
-}
-
-const timeZone = () => {
-  return body('timeZone')
-    .optional()
-    .notEmpty()
-    .withMessage('Time zone param is empty!')
-    .isInt()
-    .withMessage('Time zone must be a number')
-}
-
-const updatePersonalInfoFields = [
-  lastName(),
-  phone(),
-  birthDate(),
-  address(),
-  city(),
-  country(),
-  timeZone(),
-]
-
-module.exports = updatePersonalInfoFields
+module.exports = { persInfoSchema }
