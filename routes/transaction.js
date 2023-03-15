9a23d4948308948439d308629291dac3548af5bb
DanNisenson <danpablo1@gmail.com>
2023-03-15 01:08:12 +0100

    🧹Delete unused model files

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,10 +3,6 @@ var router = express.Router();
 const { validateTransaction } = require('../middlewares/TransactionValidator');
 const { validateResult } = require('../middlewares/validators/bodyValid');
 
-// router.get('/', require("../controllers/Transactions/getAllTransactions"));
-
-// router.get('/', require("../controllers/Transactions/getTransaction"));
-
 // router.post('/:selleruuid/:totalamount', validateTransaction, validateResult, require("../controllers/Transactions/getNewTransaction"));
 
 module.exports = router;
