527533d7a9df03d526d88a1dac818552df18e14b
DanNisenson <danpablo1@gmail.com>
2023-03-15 14:20:58 +0100

    ✨Update validator to remove unwanted fields from body

--- middlewares/validators/bodyValid.js
+++ middlewares/validators/bodyValid.js
@@ -1,12 +1,19 @@
-const { validationResult } = require('express-validator');
+const { validationResult, matchedData } = require('express-validator');
 
 const validateResult = (req, res, next) => {
+    removeUnwantedFields(req)
+
     try{
         validationResult(req).throw()
         return next()
     } catch (err) {
         res.status(400).send({ errors: err.array() })
     }
+}
+
+const removeUnwantedFields = (req) => {
+    const validData = matchedData(req, { includeOptionals: false })
+    req.body = validData
 }
 
 module.exports = { validateResult }
